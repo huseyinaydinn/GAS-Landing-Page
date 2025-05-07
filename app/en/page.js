491fdb34c4a60ed8a14f16631e5d738f73085e5b
Hüseyin Aydın <hsynaydino@gmail.com>
2025-05-07 07:34:10 +0300

    Founders - DemoStartButtons Companent created

--- app/en/page.js
+++ app/en/page.js
@@ -1,29 +1,28 @@
-
 import Hero from "../../components/Hero/page";
 import ValuePillars from "../../components/ValuePillars/page";
 import NumbersThatSpeak from "../../components/NumbersThatSpeak/page";
-import ChooseScenario from "../../components/ChooseScenario/page"
-import ExperiencePlatform from "../../components/ExperiencePlatform/page"
-import DataTools from "../../components/DataTools/page"
-import SpecificSolutions from "../../components/SpecificSolutions/page"
-import SuccessStories from "../../components/SuccessStories/page"
-import Registration from "../../components/Registration/page"
-import Founders from "../../components/Founders/page"
-
+import ChooseScenario from "../../components/ChooseScenario/page";
+import ExperiencePlatform from "../../components/ExperiencePlatform/page";
+import DataTools from "../../components/DataTools/page";
+import SpecificSolutions from "../../components/SpecificSolutions/page";
+import SuccessStories from "../../components/SuccessStories/page";
+import Registration from "../../components/Registration/page";
+import Founders from "../../components/Founders/page";
+import DemoStartButtons from "../../components/DemoStartButtons/page";
 export default function LandingTR() {
-
   return (
-<>
-  <Hero/> 
-  <ValuePillars />
-  <NumbersThatSpeak />
-  <ChooseScenario />
-  <ExperiencePlatform />
-  <DataTools />
-  <SpecificSolutions />
-  <SuccessStories />
-  <Registration />
-  <Founders />
-</>
+    <>
+      <Hero />
+      <ValuePillars />
+      <NumbersThatSpeak />
+      <ChooseScenario />
+      <ExperiencePlatform />
+      <DataTools />
+      <SpecificSolutions />
+      <SuccessStories />
+      <Registration />
+      <Founders />
+      <DemoStartButtons />
+    </>
   );
 }
--- components/Founders/page.jsx
+++ components/Founders/page.jsx
@@ -4,12 +4,13 @@ import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { FaLinkedin, FaInstagram, FaGithub, FaChevronLeft, FaChevronRight } from "react-icons/fa";
 import "swiper/css";
+import Link from "next/link";
 
 const founders = [
   {
     name: "Redif Ekinci",
     title: "CEO & Founder",
-    image: "/Founders/RedifEkinci.jpg",
+    image: "/Founders/RedifEkinci.png",
     linkedin: "https://www.linkedin.com/in/redifekinci/",
     instagram: "https://www.instagram.com/redifekinci/",
     github: "",
@@ -18,7 +19,7 @@ const founders = [
   {
     name: "Abdullah Artem",
     title: "Chief Marketing Officer",
-    image: "/Founders/AbdullahArtem.jpg",
+    image: "/Founders/AbdullahArtem.png",
     linkedin: "https://www.linkedin.com/in/abdullahartem/",
     instagram: "https://www.instagram.com/abdullahartem/",
     github: "",
@@ -27,7 +28,7 @@ const founders = [
   {
     name: "Abdulrahman Dergham",
     title: "Chief Product Officer",
-    image: "/Founders/AbdulrahmanDergham.jpg",
+    image: "/Founders/AbdulrahmanDergham.png",
     linkedin: "https://www.linkedin.com/in/abdulrahmandergham/",
     instagram: "",
     github: "https://github.com/abdulrahmandergham",
@@ -36,28 +37,28 @@ const founders = [
   {
     name: "Hassan Farooq",
     title: "CTO",
-    image: "/Founders/HassanFarooq.jpg",
+    image: "/Founders/HassanFarooq.png",
     linkedin: "https://www.linkedin.com/in/hassanfarooq/",
     instagram: "",
     github: "https://github.com/hassanfarooq",
-    desc: "AI won’t take your job. But it will crush your pipeline, hallucinate at scale, and still get more budget than you." 
+    desc: "AI won’t take your job. But it will crush your pipeline, hallucinate at scale, and still get more budget than you."
   },
 ];
 
 export default function Founders() {
   return (
     <section className="py-16">
-      <div className="max-w-6xl mx-auto px-4">
-        <h2 className="text-center text-lg md:text-xl font-light text-gray-200 mb-4">
+      <div className="max-w-6xl mx-auto px-4 flex flex-col gap-8">
+        <h2 className="text-center text-lg md:text-xl font-extralight text-gray-500 mb-4">
           The Founders Behind the Platform the Market Was Missing.
         </h2>
-        <h3 className="text-center text-2xl md:text-3xl font-bold mb-10 text-blue-200">
+        <h3 className="text-2xl sm:text-3xl md:text-4xl font-light text-center leading-tight bg-gradient-to-r from-white to-[#A6CCFF] bg-clip-text text-transparent">
           We didn’t build this to chase trends. We built it because nothing else worked the way we needed.<br />
           Now it’s yours to use — and scale with.
         </h3>
         <div className="relative flex items-center">
           <button className="founder-swiper-prev absolute left-[-10px] top-1/2 -translate-y-1/2 z-20 text-white rounded-full w-10 h-10 flex items-center justify-center text-2xl font-bold shadow-md hover:scale-110 transition xl:hidden">
-          <FaChevronLeft />
+            <FaChevronLeft />
           </button>
           <Swiper
             spaceBetween={8}
@@ -79,44 +80,44 @@ export default function Founders() {
                   className="flex flex-col justify-between w-64 h-72 md:w-[200px] md:h-80 rounded-2xl border border-[#23252C] shadow-xl relative mx-auto bg-cover bg-center overflow-hidden"
                   style={{ backgroundImage: `url(${founder.image})` }}
                 >
+                  {/* Gradient overlay for better text visibility */}
+                  <div className="absolute bottom-0 left-0 right-0 h-[150px] bg-gradient-to-t from-black/90 to-transparent z-0"></div>
                   {/* Social Icons */}
-                  <div className="absolute top-3 left-3 z-10 flex gap-2">
+                  <div className="absolute top-3 left-3 z-10 flex gap-2 ">
                     {founder.linkedin && (
-                      <a href={founder.linkedin} target="_blank" rel="noopener noreferrer" className="bg-white/10 hover:bg-blue-500 text-blue-400 hover:text-white p-2 rounded-lg transition">
+                      <Link href={founder.linkedin} target="_blank" rel="noopener noreferrer" className="bg-white/10 text-white hover:text-white p-2 rounded-lg transition cursor-pointer">
                         <FaLinkedin size={16} />
-                      </a>
+                      </Link>
                     )}
                     {founder.instagram && (
-                      <a href={founder.instagram} target="_blank" rel="noopener noreferrer" className="bg-white/10 hover:bg-pink-500 text-pink-400 hover:text-white p-2 rounded-lg transition">
+                      <Link href={founder.instagram} target="_blank" rel="noopener noreferrer" className="bg-white/10 text-white hover:text-white p-2 rounded-lg transition cursor-pointer">
                         <FaInstagram size={16} />
-                      </a>
+                      </Link>
                     )}
                     {founder.github && (
-                      <a href={founder.github} target="_blank" rel="noopener noreferrer" className="bg-white/10 hover:bg-gray-800 text-gray-300 hover:text-white p-2 rounded-lg transition">
+                      <Link href={founder.github} target="_blank" rel="noopener noreferrer" className="bg-white/10 text-white hover:text-white p-2 rounded-lg transition cursor-pointer">
                         <FaGithub size={16} />
-                      </a>
+                      </Link>
                     )}
                   </div>
                   {/* Info */}
-                  <div className="flex-1 flex flex-col justify-end z-10 p-4 md:p-6">
-                    <div className="text-white text-[16px] font-semibold mb-1 drop-shadow-lg">{founder.name}</div>
-                    <div className="text-xs text-blue-200 font-light mb-2 drop-shadow">{founder.title}</div>
+                  <div className="flex-1 flex flex-col justify-end z-10 p-2 md:p-2 pb-4 md:pb-6">
+                    <div className="text-white text-[18px] text-nowrap font-light mb-1 drop-shadow-lg">{founder.name}</div>
+                    <div className="text-sm text-gray-300 font-extralight mb-2 drop-shadow">{founder.title}</div>
+                  </div>
+                </div>
+                {/* Her kartın kendi açıklama alanı */}
+                <div className="w-full mt-3">
+                  <div className="text-xs text-gray-300 font-light text-center min-h-[32px] w-full max-w-[16rem] md:max-w-[18rem] mx-auto">
+                    {founder.desc}
                   </div>
                 </div>
               </SwiperSlide>
             ))}
           </Swiper>
           <button className="founder-swiper-next absolute right-[-10px] top-1/2 -translate-y-1/2 z-20 cursor-pointer text-white rounded-full w-10 h-10 flex items-center justify-center text-2xl font-bold shadow-md hover:scale-110 transition xl:hidden">
-          <FaChevronRight /> 
+            <FaChevronRight />
           </button>
-        </div>
-        {/* Desc kutusu Swiper'ın altında ve ortalanmış */}
-        <div className="flex justify-center items-center mt-6">
-          {/* Aktif founder'ın desc'i burada gösterilecek (JS ile) */}
-          {/* Bunu ileride state ile bağlayabiliriz, şimdilik örnek olarak ilk founder'ın desc'i */}
-          <div className="text-xs text-gray-300 font-light text-center min-h-[32px] w-full max-w-[16rem] md:max-w-[18rem]">
-            {founders[0].desc}
-          </div>
         </div>
       </div>
     </section>
